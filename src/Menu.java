a5b3ce6acab570d77075c8787e1ad6d8e100cb12
Zahra Rafiei <z.rafiei.82@gmail.com>
2023-04-07 07:37:21 +0330

    sign up completed

--- src/Menu.java
+++ src/Menu.java
@@ -5,37 +5,75 @@ public class Menu {
     private static final Scanner sc = new Scanner(System.in);
     public static ArrayList<String> username = new ArrayList<>();
     public static ArrayList<String> password = new ArrayList<>();
+    private static final String CYAN_BOLD = "\033[1;36m";
+    private static final String RESET = "\033[0m";
 
     public static void main(String[] args) {
-        mainMenu();
-        System.out.println("> Enter your command");
-        int command = sc.nextInt();
-        switch (command) {
-            case 1:
-                System.out.println("> Enter your username: ");
-                String user = sc.next();
-
-                System.out.println("> Enter your password: ");
-                String pass = sc.next();
-
-                if (user.equals("Admin") && pass.equals("Admin"))
-                {
-                    clearScreen();
-                    adminMenu();
-                } else if (username.contains(user) && password.contains(pass)) {
-                    clearScreen();
-                    passengerMenu();
-                } else {
-                    System.out.println("> Invalid username or password");
-                }
-                break;
-            case 2:
-                Signup signup = new Signup();
-        }
-
+        menuExecution();
     }
 
-    public static void mainMenu() {
+    public static void menuExecution() {
+        clearScreen();
+        mainMenu();
+        System.out.println();
+        System.out.println("> Enter your command: ");
+        int command = sc.nextInt();
+
+        while (true) {
+            switch (command) {
+                case 1 -> {
+                    clearScreen();
+                    signIn();
+                }
+                case 2 -> {
+                    clearScreen();
+                    Signup signup = new Signup();
+                    pressEnterToContinue();
+                    menuExecution();
+                }
+            }
+        }
+    }
+
+    private static void signIn() {
+        System.out.println(CYAN_BOLD + """
+
+                                                                                               \s
+                                                                                               \s
+                                                                                               \s
+                              ,--,                                   ,--,                      \s
+                            ,--.'|                   ,---,         ,--.'|         ,---,        \s
+                  .--.--.   |  |,     ,----._,.  ,-+-. /  |        |  |,      ,-+-. /  |       \s
+                 /  /    '  `--'_    /   /  ' / ,--.'|'   |        `--'_     ,--.'|'   |       \s
+                |  :  /`./  ,' ,'|  |   :     ||   |  ,"' |        ,' ,'|   |   |  ,"' |       \s
+                |  :  ;_    '  | |  |   | .\\  .|   | /  | |        '  | |   |   | /  | |       \s
+                 \\  \\    `. |  | :  .   ; ';  ||   | |  | |        |  | :   |   | |  | |       \s
+                  `----.   \\'  : |__'   .   . ||   | |  |/         '  : |__ |   | |  |/        \s
+                 /  /`--'  /|  | '.'|`---`-'| ||   | |--'          |  | '.'||   | |--'         \s
+                '--'.     / ;  :    ;.'__/\\_: ||   |/              ;  :    ;|   |/             \s
+                  `--'---'  |  ,   / |   :    :'---'               |  ,   / '---'              \s
+                             ---`-'   \\   \\  /                      ---`-'                     \s
+                                       `--`-'                                                  \s
+                """ + RESET);
+        System.out.println("> Enter your username: ");
+        String user = sc.next();
+        System.out.println("> Enter your password: ");
+        String pass = sc.next();
+        if (user.equals("Admin") && pass.equals("Admin")) {
+            clearScreen();
+            adminMenu();
+        } else if (username.contains(user) && password.contains(pass)) {
+            clearScreen();
+            passengerMenu();
+        } else {
+            System.out.println("> Invalid username or password");
+            System.out.println("> If you don't hava an account, create one in sign up menu...");
+            pressEnterToContinue();
+            menuExecution();
+        }
+    }
+
+    private static void mainMenu() {
         System.out.println("""
                 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
                            WELCOME TO AIRLINE RESERVATION SYSTEM
@@ -46,7 +84,7 @@ public class Menu {
                     <2> Sign up\s""");
     }
 
-    public static void adminMenu() {
+    private static void adminMenu() {
         System.out.println("""
                 ::::::::::::::::::::::::::::::::::::::::
                            Admin MENU OPTIONS
@@ -59,7 +97,7 @@ public class Menu {
                     <0> Sign out""");
     }
 
-    public static void passengerMenu() {
+    private static void passengerMenu() {
         System.out.println("""
                 ::::::::::::::::::::::::::::::::::::::::
                          PASSENGER MENU OPTIONS
@@ -74,7 +112,17 @@ public class Menu {
                     <0> Sign out""");
     }
 
-    public static void clearScreen() {
+    private static void clearScreen() {
         System.out.print("\033[H\033[2J");
+    }
+
+    private static void pressEnterToContinue() {
+        System.out.printf("%n%s%n", "Press Enter key to continue...");
+        try {
+            sc.nextLine();
+            sc.nextLine();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/Signup.java
+++ src/Signup.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 import java.util.regex.*;
 
 public class Signup {
-    public static final String RESET = "\033[0m";
-    public static final String RED_BOLD = "\033[1;31m";
-    public static final String GREEN_BOLD = "\033[1;32m";
+    private static final String RESET = "\033[0m";
+    private static final String RED_BOLD = "\033[1;31m";
+    private static final String GREEN_BOLD = "\033[1;32m";
+    private static final String CYAN_BOLD = "\033[1;36m";
+    private static final String TEXT_ITALIC      = "\033[3m";
     private static final Scanner sc = new Scanner(System.in);
 
     private final String regex = "^(?=.*[a-z])(?=."
@@ -13,10 +15,35 @@ public class Signup {
     Pattern pattern = Pattern.compile(regex);
 
     public Signup() {
-        System.out.printf("%n%s%n", ">> Username must contain" + RED_BOLD + " capital " + RESET + "letters," + RED_BOLD + " small " + RESET + "letters and" + RED_BOLD + " numbers " + RESET + "with the length of at least" + RED_BOLD + " 4 digits" + RESET + ": ");
+        System.out.println("\n" + CYAN_BOLD +
+                "                                                                            \n" +
+                "                                                                            \n" +
+                "                                                                            \n" +
+                "              ,--,                                               ,-.----.   \n" +
+                "            ,--.'|                   ,---,                  ,--, \\    /  \\  \n" +
+                "  .--.--.   |  |,     ,----._,.  ,-+-. /  |               ,'_ /| |   :    | \n" +
+                " /  /    '  `--'_    /   /  ' / ,--.'|'   |          .--. |  | : |   | .\\ : \n" +
+                "|  :  /`./  ,' ,'|  |   :     ||   |  ,\"' |        ,'_ /| :  . | .   : |: | \n" +
+                "|  :  ;_    '  | |  |   | .\\  .|   | /  | |        |  ' | |  . . |   |  \\ : \n" +
+                " \\  \\    `. |  | :  .   ; ';  ||   | |  | |        |  | ' |  | | |   : .  | \n" +
+                "  `----.   \\'  : |__'   .   . ||   | |  |/         :  | : ;  ; | :     |`-' \n" +
+                " /  /`--'  /|  | '.'|`---`-'| ||   | |--'          '  :  `--'   \\:   : :    \n" +
+                "'--'.     / ;  :    ;.'__/\\_: ||   |/              :  ,      .-./|   | :    \n" +
+                "  `--'---'  |  ,   / |   :    :'---'                `--`----'    `---'.|    \n" +
+                "             ---`-'   \\   \\  /                                     `---`    \n" +
+                "                       `--`-'                                               \n" + RESET);
+
+        System.out.println();
+        System.out.println(TEXT_ITALIC + RED_BOLD + """
+                ::::::::::::::::::::::::::::::::::::::::::::::::::::
+                username and password must contain the followings:
+                >> Capital letters
+                >> Small letters
+                >> numbers
+                >> with the length of at least 4 digits
+                ::::::::::::::::::::::::::::::::::::::::::::::::::::""" + RESET);
         System.out.println("> Enter your username: ");
         setUsername(sc.nextLine());
-        System.out.printf("%n%s%n", ">> Password must contain" + RED_BOLD + " capital " + RESET + "letters," + RED_BOLD + " small " + RESET + "letters and" + RED_BOLD + " numbers " + RESET + "with the length of at least" + RED_BOLD + " 4 digits" + RESET + ": ");
         System.out.println("> Enter your password: ");
         setPassword(sc.nextLine());
     }
@@ -25,14 +52,14 @@ public class Signup {
         Matcher matcher = pattern.matcher(newUsername);
 
         if (Menu.username.contains(newUsername)) {
-            System.out.println("> Duplicated username...");
-            System.out.println("> Enter a valid username: ");
+            System.out.println(">" + RED_BOLD + "Duplicated username..." + RESET);
+            System.out.println("> Enter another username: ");
             this.setUsername(sc.nextLine());
         } else if ((newUsername.length() >= 4) && matcher.matches()) {
             Menu.username.add(newUsername);
             System.out.printf("%s%n%n", GREEN_BOLD + "> Username added successfully" + RESET);
         } else {
-            System.out.println("> Invalid username...");
+            System.out.println(">" + RED_BOLD + "Invalid username..." + RESET);
             System.out.println("> Enter a valid username: ");
             this.setUsername(sc.nextLine());
         }
@@ -42,14 +69,14 @@ public class Signup {
         Matcher matcher = pattern.matcher(newPassword);
 
         if (Menu.password.contains(newPassword)) {
-            System.out.println("> Duplicated password...");
-            System.out.println("> Enter a valid password: ");
+            System.out.println(">" + RED_BOLD + "Duplicated password..." + RESET);
+            System.out.println("> Enter another password: ");
             this.setPassword(sc.next());
         } else if ((newPassword.length() >= 4) && matcher.matches()) {
             Menu.password.add(newPassword);
-            System.out.printf("%s%n%n", GREEN_BOLD + "> Account created successfully" + RESET);
+            System.out.printf("%s%n", GREEN_BOLD + "> Account created successfully" + RESET);
         } else {
-            System.out.println("> Invalid password...");
+            System.out.println(">" + RED_BOLD + "Invalid password..." + RESET);
             System.out.println("> Enter a valid password: ");
             this.setPassword(sc.nextLine());
         }
