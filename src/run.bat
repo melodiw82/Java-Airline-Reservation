def19d29730e1f154b4fe0acfa458a9b35f60df2
Zahra Rafiei <z.rafiei.82@gmail.com>
2023-05-09 22:17:02 +0330

    run deleted

--- src/run.bat
+++
@@ -1,4 +0,0 @@
-mode con:cols=125 lines=30
-G:\Computer\Java\jdk19\bin\java.exe "-javaagent:G:\Computer\Java\IntelliJ IDEA 2022.2.1\lib\idea_rt.jar=11478:G:\Computer\Java\IntelliJ IDEA 2022.2.1\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath "G:\airline_reservation\out\production\airline_reservation;C:\Program Files (x86)\MySQL\Connector J 8.0\mysql-connector-j-8.0.33.jar" Main
-
-pause
